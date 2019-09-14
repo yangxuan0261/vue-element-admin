962511dda735adecf892cdf77b606c38f9198e0e
wilker <364105996@qq.com>
2019-09-14 23:21:28 +0800

    -a

--- src/views/permission/role.vue
+++ src/views/permission/role.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="app-container">
+    <div class="demo-image__preview">
+      <el-image
+        style="width: 100px; height: 100px"
+        :src="url"
+        :preview-src-list="srcList"
+      />
+    </div>
     <el-button type="primary" @click="handleAddRole">New Role</el-button>
 
     <el-table :data="rolesList" style="width: 100%;margin-top:30px;" border>
@@ -83,7 +90,12 @@ export default {
       defaultProps: {
         children: 'children',
         label: 'title'
-      }
+      },
+      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+      srcList: [
+        'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
+        'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg'
+      ]
     }
   },
   computed: {
